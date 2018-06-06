134cfc1ea45fe10911dd1cefd7f38b6b0e96a68c
Ryan S. White <sunsetquest@hotmail.com>
2018-06-05 22:13:47 -0700

    Initial Release

---
+++ OutlookSignatureTool/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace CreateOutlookSignatureTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webBrowserPreview = new System.Windows.Forms.WebBrowser();
+            this.label8 = new System.Windows.Forms.Label();
+            this.btnApplyToOutlook = new System.Windows.Forms.Button();
+            this.flow = new System.Windows.Forms.FlowLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.cboTemplate = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // webBrowserPreview
+            // 
+            this.webBrowserPreview.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowserPreview.Location = new System.Drawing.Point(0, 0);
+            this.webBrowserPreview.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowserPreview.Name = "webBrowserPreview";
+            this.webBrowserPreview.Size = new System.Drawing.Size(518, 370);
+            this.webBrowserPreview.TabIndex = 10;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(254, 133);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(0, 13);
+            this.label8.TabIndex = 13;
+            // 
+            // btnApplyToOutlook
+            // 
+            this.btnApplyToOutlook.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.btnApplyToOutlook.Location = new System.Drawing.Point(0, 559);
+            this.btnApplyToOutlook.Name = "btnApplyToOutlook";
+            this.btnApplyToOutlook.Size = new System.Drawing.Size(518, 34);
+            this.btnApplyToOutlook.TabIndex = 16;
+            this.btnApplyToOutlook.Text = "Apply To Outlook";
+            this.btnApplyToOutlook.UseVisualStyleBackColor = true;
+            this.btnApplyToOutlook.Click += new System.EventHandler(this.btnApplyToOutlook_Click);
+            // 
+            // flow
+            // 
+            this.flow.AutoScroll = true;
+            this.flow.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flow.Location = new System.Drawing.Point(0, 0);
+            this.flow.Name = "flow";
+            this.flow.Size = new System.Drawing.Size(518, 140);
+            this.flow.TabIndex = 17;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.cboTemplate);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(518, 45);
+            this.panel1.TabIndex = 18;
+            // 
+            // cboTemplate
+            // 
+            this.cboTemplate.FormattingEnabled = true;
+            this.cboTemplate.Location = new System.Drawing.Point(65, 18);
+            this.cboTemplate.Name = "cboTemplate";
+            this.cboTemplate.Size = new System.Drawing.Size(230, 21);
+            this.cboTemplate.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(5, 21);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(51, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Template";
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 45);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.flow);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.webBrowserPreview);
+            this.splitContainer1.Size = new System.Drawing.Size(518, 514);
+            this.splitContainer1.SplitterDistance = 140;
+            this.splitContainer1.TabIndex = 19;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(518, 593);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnApplyToOutlook);
+            this.Controls.Add(this.label8);
+            this.Name = "Form1";
+            this.Text = "Outlook Signature Tool";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.WebBrowser webBrowserPreview;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button btnApplyToOutlook;
+        private System.Windows.Forms.FlowLayoutPanel flow;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ComboBox cboTemplate;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+    }
+}
+
---
+++ OutlookSignatureTool/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+//%username%\AppData\Roaming\Microsoft\Signatures
+
+
+
+namespace CreateOutlookSignatureTool
+{
+
+
+    public partial class Form1 : Form
+    {
+        List<Templete> templates = new List<Templete>();
+        string currentTempleteHTML;
+        List<FieldsInTemplete> currentFields = new List<FieldsInTemplete>();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //todo: make sure no templates works
+            string[] paths = Directory.GetFiles(Directory.GetCurrentDirectory() + "\\Templates", "*.htm");
+            foreach (var path in paths)
+            {
+
+                string nameToAdd = Path.GetFileNameWithoutExtension(path);
+                // string nameToAdd = Regex.Match(path, @"[^\\]*(?=.htm$)").Value; // another method
+                string pathWithoutExt = Path.Combine(Path.GetDirectoryName(path), nameToAdd);
+                templates.Add( new Templete
+                {
+                    FilePath = pathWithoutExt,
+                    Name = nameToAdd
+                });
+            }
+
+            cboTemplate.DataSource = new BindingSource(templates,null);
+            cboTemplate.DisplayMember = "Name";
+            cboTemplate.SelectionChangeCommitted += (sender1, e1) => NewTempleteSelected();
+
+            NewTempleteSelected();
+        }
+
+
+        void NewTempleteSelected()
+        {
+            // Lets get the text
+            string path = (cboTemplate.SelectedValue as Templete).FilePath + ".htm";
+            currentTempleteHTML = File.ReadAllText(path);
+            currentTempleteHTML= Regex.Replace(currentTempleteHTML, @"[^\u0000-\u007F]+", string.Empty);
+
+            // Clear the current fields
+            currentFields.Clear();
+
+            // Goes through text and finds the fillable fields 
+            MatchCollection matches = Regex.Matches(currentTempleteHTML, @"(?:\{\*)([^\{\}]{1,15})(?:\*\})");
+
+            flow.Controls.Clear();
+
+            foreach (Match match in matches)
+            {
+                foreach (Capture capture in match.Captures)
+                {
+                    //Console.WriteLine("Index={0}, Value={1}", capture.Index, capture.Value);
+                    Label lbl = new Label { Text = match.Groups[1].Value + ":", AutoSize = true};
+                    flow.Controls.Add(lbl);
+                    TextBox txt = new TextBox { /*Width = flow.Width - 120*/ };
+                    txt.TextChanged += (s, e) => RefreshPreview(); 
+                    flow.Controls.Add(txt);
+                    flow.SetFlowBreak(txt, true);
+
+                    var newField = new FieldsInTemplete { txtBox = txt, Name = match.Value };
+                    txt.Tag = newField;
+                    currentFields.Add(newField);
+                }
+            }
+
+            RefreshPreview();
+        }
+
+        private void RefreshPreview()
+        {
+            string newHTML = currentTempleteHTML;
+             newHTML = Regex.Replace(newHTML, @"[^\u0000-\u007F]+", string.Empty);
+            foreach (var field in currentFields)
+            {
+                //if (!string.IsNullOrEmpty(field.txtBox.Text))
+                    newHTML = newHTML.Replace(field.Name, field.txtBox.Text);
+            }
+            // Lets update the preview window
+            webBrowserPreview.DocumentText = newHTML;
+        }
+
+        private void btnApplyToOutlook_Click(object sender, EventArgs e)
+        {
+            string name = (cboTemplate.SelectedValue as Templete).Name;
+            string path = (cboTemplate.SelectedValue as Templete).FilePath;
+            //path = Path.Combine(path, name);
+
+            string newHTM = File.ReadAllText(path + ".htm");
+            string newRTF = File.ReadAllText(path + ".rtf");
+            string newTXT = File.ReadAllText(path + ".txt");
+
+            newHTM = Regex.Replace(newHTM, @"[^\u0000-\u007F]+", string.Empty);
+            newRTF = Regex.Replace(newRTF, @"[^\u0000-\u007F]+", string.Empty);
+            newTXT = Regex.Replace(newTXT, @"[^\u0000-\u007F]+", string.Empty);
+
+            foreach (var field in currentFields)
+            {
+                newHTM = newHTM.Replace(field.Name, field.txtBox.Text);
+                newRTF = newRTF.Replace(field.Name, field.txtBox.Text);
+                newTXT = newTXT.Replace(field.Name, field.txtBox.Text);
+            }
+            
+            string SignaturesPath = Path.Combine(
+                Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+                "Microsoft","Signatures");
+
+            string htmPath = Path.Combine(SignaturesPath, name + ".htm");
+            File.WriteAllText(htmPath, newHTM);
+
+            string rtfPath = Path.Combine(SignaturesPath, name + ".rtf");
+            File.WriteAllText(rtfPath, newRTF);
+
+            string txtPath = Path.Combine(SignaturesPath, name + ".txt");
+            File.WriteAllText(txtPath, newTXT);
+
+            //Now Create the folder
+            string newPath = Path.Combine(SignaturesPath, name + "_files");
+            if (!Directory.Exists(newPath))
+                Directory.CreateDirectory(newPath);
+
+            //Copy all the files & Replaces any files with the same name
+            //string supportFiles = Path.Combine(SignaturesPath, path + "_files");
+            foreach (var source in Directory.GetFiles(path + "_files"))
+            {
+                string target = Path.Combine(newPath, Path.GetFileName(source));
+                File.Copy(source, target, true);
+            }
+
+
+
+        }
+
+
+    }
+
+    class Templete
+    {
+        public string Name { get; set; }
+        public string FilePath;
+    }
+
+    class FieldsInTemplete
+    {
+        public string Name { get; set; }
+        public TextBox txtBox;
+    }
+}
